41ca4af7344aa07c2f04b2e0bb080124c32eafa4
denis <dens05.04@mail.ru>
2025-04-09 19:24:11 +0300

    The function of filling out a medical card during registration has been implemented

--- src/main/java/ru/ffanjex/backenddevelopment/controller/AuthController.java
+++ src/main/java/ru/ffanjex/backenddevelopment/controller/AuthController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.ffanjex.backenddevelopment.dto.JwtResponse;
+import ru.ffanjex.backenddevelopment.dto.MedicalCardDTO;
 import ru.ffanjex.backenddevelopment.dto.UserLoginDTO;
 import ru.ffanjex.backenddevelopment.dto.UserRegistrationDTO;
+import ru.ffanjex.backenddevelopment.service.MedicalCardService;
 import ru.ffanjex.backenddevelopment.service.UserService;
 
 @RestController
@@ -15,11 +17,20 @@ import ru.ffanjex.backenddevelopment.service.UserService;
 public class AuthController {
 
     private final UserService userService;
+    private final MedicalCardService medicalCardService;
 
-    @PostMapping("/register")
+    @PostMapping("/register/users")
     public ResponseEntity<?> register(@Valid @RequestBody UserRegistrationDTO dto) {
         userService.registerUser(dto);
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/register/medical_card")
+    public ResponseEntity<?> registerMedicalCard(@Valid @RequestBody MedicalCardDTO dto,
+                                                 @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        MedicalCardDTO createdCard = medicalCardService.createMedicalCard(dto, token);
+        return ResponseEntity.ok(createdCard);
     }
 
     @PostMapping("/login")
