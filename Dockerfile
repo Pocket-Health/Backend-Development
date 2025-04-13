7ea726d37d0c54ed9226611fcc9b912981729048
U0Nkis <trufanov-e95@mail.ru>
2025-04-13 22:51:36 +0300

    Fixed pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,22 @@
-FROM openjdk:17
-VOLUME /tmp
-EXPOSE 8081
-COPY target/*.jar app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+#Build
+FROM maven:3.9-amazoncorretto-17 AS build
+WORKDIR /build
+COPY pom.xml .
+RUN mvn dependency:go-offline
+COPY src ./src
+RUN mvn clean package -DskipTests
+
+#Runtime
+FROM amazoncorretto:17
+#define
+ARG APP_VERSION=0.0.1-SNAPSHOT
+
+WORKDIR /app
+COPY --from=build /build/target/Backend-Development-*.jar /app/
+
+EXPOSE 8080
+
+ENV DB_URL=jdbc:postgresql://postgres-sql-ph:5432/medical_app
+ENV JAR_VERSION=${APP_VERSION}
+
+CMD java -jar -Dspring.datasource.url=${DB_URL} Backend-Development-${JAR_VERSION}.jar
---
+++ db/init.sql
@@ -0,0 +1,43 @@
+CREATE TABLE users (
+                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       notifications_enabled BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE medical_cards (
+                               id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                               user_id UUID UNIQUE NOT NULL,
+                               full_name VARCHAR(255) NOT NULL,
+                               height INT,
+                               weight DECIMAL(5, 2),
+                               blood_type VARCHAR(10) NOT NULL,
+                               allergies TEXT,
+                               diseases TEXT,
+                               FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE medication_schedule (
+                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                     user_id UUID NOT NULL,
+                                     medication_name VARCHAR(255) NOT NULL,
+                                     scheduled_days INTEGER[] NOT NULL,
+                                     scheduled_times TIME[] NOT NULL,
+                                     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE chat_history (
+                              id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                              user_id UUID UNIQUE NOT NULL,
+                              message TEXT NOT NULL,
+                              FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE knowledge_base (
+                                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                disease_name VARCHAR(255) NOT NULL,
+                                description TEXT NOT NULL,
+                                types TEXT NOT NULL,
+                                symptoms TEXT NOT NULL,
+                                causes TEXT NOT NULL
+);
