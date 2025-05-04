06003d4bcc86393a06729023b65109a4f13bc618
denis <dens05.04@mail.ru>
2025-05-04 15:52:06 +0300

    Correction of a situation in which the user could not make requests that include JWT

--- src/main/java/ru/ffanjex/backenddevelopment/config/SwaggerConfig.java
+++ src/main/java/ru/ffanjex/backenddevelopment/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package ru.ffanjex.backenddevelopment.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +26,15 @@ public class SwaggerConfig {
                         .title("Backend API")
                         .description("API documentation for the medical application")
                         .version("1.0.0")
-                );
+                )
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
